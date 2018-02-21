300d824feb102eed8f55b008989a29eb9887097b
Manoj Panta <manojpanta95@gmail.com>
2018-02-21 10:34:53 -0700

    test for hiking method

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,13 @@
 class Activity
+
+
+  def initialize
+
+
+  end
+
+  def hiking(participants)
+    participants.values.sum
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -5,11 +5,18 @@ require './lib/activity'
 
 class ActivityTest < Minitest::Test
 def setup
-  activity = Activity.new
-end
+  @activity = Activity.new
+ end
 
-
-end
+#
+# end
 def test_Instance_of_activity
-  assert_instnace_of Activity, activity
+  assert_instance_of Activity, @activity
+end
+
+def test_hiking_method
+  participants = {:manoj => 14, :nick => 11 }
+  assert_equal 25, @activity.hiking(participants)
+end
+
 end
