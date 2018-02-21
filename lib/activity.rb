d0509443c769c1e94c623f903ba900fee5576af2
Manoj Panta <manojpanta95@gmail.com>
2018-02-21 10:55:59 -0700

    calculating the split

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,13 +1,22 @@
 class Activity
+  # attr_reader :total_cost
 
 
   def initialize
-
-
+    @total_cost = 0
+    @participants = 0
   end
 
   def hiking(participants)
-    participants.values.sum
+    @total_cost = participants.values.sum
+    @participants = participants.keys.length
+
+  end
+  # require 'pry' ; binding.pry
+  def split_cost
+    @total_cost.to_f/@participants.length
+
+  #   hiking
   end
 
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -6,17 +6,20 @@ require './lib/activity'
 class ActivityTest < Minitest::Test
 def setup
   @activity = Activity.new
+  @participants = {:manoj => 14, :nick => 11 }
  end
-
-#
-# end
 def test_Instance_of_activity
   assert_instance_of Activity, @activity
 end
 
 def test_hiking_method
-  participants = {:manoj => 14, :nick => 11 }
-  assert_equal 25, @activity.hiking(participants)
+  assert_equal 25, @activity.hiking(@participants)
+  # require 'pry' ; binding.pry
+end
+
+def test_split
+  assert_equal 25, @activity.hiking(@participants)
+  assert_equal 12.5, @activity.split_cost
 end
 
 end
