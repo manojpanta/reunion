bfccdfcef236068b040cab89c0ca3b9be5d0a1e3
Manoj Panta <manojpanta95@gmail.com>
2018-02-21 09:56:18 -0700

    first test

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,12 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+def setup
+  activity = Activity.new
+end
+
+
+end
+def test_Instance_of_activity
+  assert_instnace_of Activity, activity
 end
